83c150aa14b9c0ceefa0a382b8eb531f5bb2114c
Pascal Ackermann <ackermann.pascal@gmail.con>
2023-10-10 07:43:05 +0200

    Implemented function that returns all information of birthday-childs

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import pandas
+import pprint
+import datetime as dt
+
+
 ##################### Hard Starting Project ######################
 
 # 1. Update the birthdays.csv with your friends & family's details. 
@@ -9,14 +14,28 @@
 # birthdays_dict = {
 #     (month, day): data_row
 # }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
+def read_birthdays_from_csv():
+    birthdays_dict = pandas.read_csv("birthdays.csv")
+    return birthdays_dict
+
+def get_birthday_childs_of_today(today):
+    day_mask = birthday_list['day'] == today.day
+    month_mask = birthday_list['month'] == today.month
+    return birthday_list[day_mask & month_mask]
+
+if __name__ == '__main__':
+    birthday_list = read_birthdays_from_csv()
+    print(get_birthday_childs_of_today(dt.datetime.now()))
+
+
+
+# HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
 # if (today_month, today_day) in birthdays_dict:
+
+
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 # HINT: https://www.w3schools.com/python/ref_string_replace.asp
 
 # 4. Send the letter generated in step 3 to that person's email address.
 # HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-
-
-
