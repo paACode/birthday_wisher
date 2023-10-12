8920943d9e24c9a8f7663b429d0d17243421eb6e
Pascal Ackermann <ackermann.pascal@gmail.con>
2023-10-12 07:44:19 +0200

    Added openAPI to generate 10 birthday letters for each month

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import pandas
 import datetime as dt
 import os
 import glob
+import openai
+import calendar
 
 
 ##################### Hard Starting Project ######################
-
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
+# Todo: Add OpenAI
+# Todo: Make sure email will be send only once a year, maybe with timestamp
+# Todo: Make it OOP
+# Todo: Add data of family and friends to list
+# Todo: Create Application and add to startup
 
 # 2. Check if today matches a birthday in the birthdays.csv
 # HINT 1: Only the month and day matter. 
@@ -25,6 +29,7 @@ def read_birthdays_from_csv():
 def get_all_birthdays_of_today(today):
     day_mask = all_birthdays['day'] == today.day
     month_mask = all_birthdays['month'] == today.month
+    print(today.month)
     return all_birthdays[day_mask & month_mask]
 
 
@@ -54,21 +59,47 @@ def get_credentials():
         credentials = file.readlines()
     my_email = credentials[0]
     my_password = credentials[1]
-    return my_email, my_password
+    my_openai_key = credentials[2]
+    return my_email, my_password, my_openai_key
+
+
+def get_keywords_of_the_month(month):
+    keywords = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct",
+        prompt=f"I need 10 words that are related to the month {month}. Output in csv format",
+        max_tokens=200
+    )
+    return keywords.choices[0].text.strip()
+
+
+def get_letter_template(including_keywords):
+    letter_template = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct",
+        prompt=f"Generate a birthday letter with [for_name] and [from_name]. Include the words {including_keywords}. Max. 3 sentences",
+        max_tokens=200
+    )
+    return letter_template.choices[0].text.strip()
+
+
+def create_letter_template_as_txt(month):
+    keywords = get_keywords_of_the_month(month)
+    template = get_letter_template(keywords)
+    with open(f'letters/{month}.txt', 'w') as f:
+        f.write(template)
+
+def create_templates_for_all_months():
+    for month in range(1,12):
+        create_letter_template_as_txt(calendar.month_name[month])
 
 
 if __name__ == '__main__':
     cwd = os.getcwd()
     all_birthdays = read_birthdays_from_csv()
     today_birthdays = get_all_birthdays_of_today(dt.datetime.now())
-    sender_email, sender_password = get_credentials()
-    for birthday in today_birthdays.itertuples():
-        letter = get_random_letter()
-        personal_letter = letter.replace("[NAME]", birthday.name)
-        write_email(personal_letter)
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
+    sender_email, sender_password, openai_key = get_credentials()
+    openai.api_key = openai_key.strip()
+    create_templates_for_all_months()
+    #create_letter_template_as_txt(calendar.month_name[dt.datetime.now().month])
 
 # 4. Send the letter generated in step 3 to that person's email address.
 # HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
